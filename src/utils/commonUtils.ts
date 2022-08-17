126fbf06f70949c8a453435e7919c066fde308c0
Chamara sanjeewa <chamara@babyjourney.se>
2022-08-17 12:30:46 +0530

    fixed minor issue

--- src/utils/commonUtils.ts
+++ src/utils/commonUtils.ts
@@ -1,5 +1,5 @@
 export const getArtistImageUrl = (artistImage: ArtistImage | undefined) => {
   //workaround to get the correct image url which comes under #text
   //@ts-ignore
-  return artistImage?.["#text"] ?? "";
+  return    artistImage?.["#text"] ?? "";
 };
